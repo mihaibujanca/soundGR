f0b4e785bf6bd477bdc479f2e75c429b519efbfb
Mihai Bujanca <mihaibujanca@github.com>
2015-02-28 15:47:23 +0000

    Fixed audio buffer

--- AudioBuffer.cpp
+++ AudioBuffer.cpp
@@ -1,29 +1,99 @@
- // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
-    2 
-    3 /*
-    4  * Copyright (C) 2013 iCub Facility - Istituto Italiano di Tecnologia
-    5  * Authors: Marco Randazzo
-    6  * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
-    7  *
-    8  */
-    9 
-   10 #include "PortAudioBuffer.h"
-   11 
-   12 #include <stdio.h>
-   13 #include <stdlib.h>
-   14 #include <portaudio.h>
-   15 
-   16 
-   17 circularBuffer::circularBuffer(int bufferSize)
-   18 {
-   19     maxsize  = bufferSize + 1; 
-   20     start = 0;
-   21     end   = 0;
-   22     elems = (SAMPLE *) calloc(maxsize, sizeof(SAMPLE));
-   23 }
-   24 
-   25 circularBuffer::~circularBuffer()
-   26 {
-   27     free(elems);
-   28 }
-   29 
+
+#include "AudioBuffer.h"
+
+// Constructor, caller can give us a hint about the number of samples we may need to hold.
+AudioBuffer::AudioBuffer(int iSizeHint)
+{
+	if (iSizeHint > 0)
+		m_vectorSamples.reserve(iSizeHint);
+
+	m_iPlaybackIter = m_vectorSamples.begin();
+}
+
+AudioBuffer::~AudioBuffer()
+{
+}
+
+int AudioBuffer::RecordCallback(const void* pInputBuffer, 
+								void* pOutputBuffer, 
+								unsigned long iFramesPerBuffer, 
+								const PaStreamCallbackTimeInfo* timeInfo, 
+								PaStreamCallbackFlags statusFlags)
+{
+	short** pData = (short**) pInputBuffer;
+
+	if (pInputBuffer == NULL)
+	{
+		cout << "AudioBuffer::RecordCallback, input buffer was NULL!" << endl;
+		return paContinue;
+	}
+
+	// Copy all the frames over to our internal vector of samples
+	for (unsigned long i = 0; i < iFramesPerBuffer; i++)
+		m_vectorSamples.push_back(pData[0][i]);
+
+	return paContinue;
+}
+
+int AudioBuffer::PlaybackCallback(const void* pInputBuffer, 
+									void* pOutputBuffer, 
+									unsigned long iFramesPerBuffer, 
+									const PaStreamCallbackTimeInfo* timeInfo, 
+									PaStreamCallbackFlags statusFlags)
+{
+	short**			pData	= (short**) pOutputBuffer;
+	unsigned long	iOutput = 0;
+
+	if (pOutputBuffer == NULL)
+	{
+		cout << "AudioBuffer::PlaybackCallback was NULL!" << endl;
+		return paComplete;
+	}
+
+	// Output samples until we either have satified the caller, or we run out
+	while (iOutput < iFramesPerBuffer)
+	{
+		if (m_iPlaybackIter == m_vectorSamples.end())
+		{
+			// Fill out buffer with zeros
+			while (iOutput < iFramesPerBuffer)
+			{
+				pData[0][iOutput] = (short) 0;
+				iOutput++;
+			}
+			return paComplete;
+		}
+
+		pData[0][iOutput] = (short) *m_iPlaybackIter;
+
+		m_iPlaybackIter++;
+		iOutput++;
+	}
+
+	return paContinue;
+}
+
+// Clear out any data in the buffer and prepare for a new recording.
+void AudioBuffer::Clear()
+{
+	m_vectorSamples.clear();
+}
+
+// Dumpt the samples to a raw file 
+void AudioBuffer::WriteToFile(const string& strFilename)
+{
+	fstream fout(strFilename.c_str(), ios::out|ios::binary);
+	short iSample;
+	for (VECTOR_SHORT_ITER iter = m_vectorSamples.begin(); iter != m_vectorSamples.end(); iter++)
+	{
+		iSample = (short) *iter;
+		fout.write((char *) &iSample, sizeof(short));
+	}
+	fout.close();
+}
+
+// Reset the playback index to the start of the samples
+void AudioBuffer::ResetPlayback()
+{
+	m_iPlaybackIter = m_vectorSamples.begin();
+}
